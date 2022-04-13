b5a486accc59c7db879302654963d3f3a96acac3
Riya Singh <Riya.Singh@ltts.com>
2022-04-13 16:13:00 +0530

    logs

--- src/main/java/com/logs/newlogs/controller/LogsController.java
+++ src/main/java/com/logs/newlogs/controller/LogsController.java
@@ -1,6 +1,7 @@
 package com.logs.newlogs.controller;
 
-import com.logs.newlogs.service.LogsService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.logging.LogLevel;
 import org.springframework.http.HttpStatus;
@@ -14,17 +15,54 @@ import java.security.Timestamp;
 @RestController
 public class LogsController {
 
-    @Autowired
-    private LogsService logService;
+    //Logger variable to create logs for logsController class
 
+    //createLogs function
+    //input:parameter send by different microservices : caller[microservice name], message[message to log], logLevel[logging level which microservice wants to create log ]
+    //output: log entry in file log4j.log
     @PostMapping("/createLogs")
-    public ResponseEntity<?> createLogs(@RequestParam("caller") String caller,@RequestParam("message") String message, @RequestParam("logLevel") LogLevel logLevel){
-        try {
+    public ResponseEntity<?> createLogs(@RequestParam("caller") String caller, @RequestParam("message") String message, @RequestParam("logLevel") LogLevel logLevel){
 
-            logService.createAllLogs(caller, message, logLevel);
+        //Logger variable to create logs for logsController class
+        Logger LOGGER = LogManager.getLogger(caller);
+        //try block
+        try {
+            //switch case to create log entry based on log level
+             switch(logLevel){
+
+                case DEBUG:
+                    //logger.log();
+                    LOGGER.debug(message);
+                    break;
+
+                case ERROR:
+                case FATAL:
+                    LOGGER.error(message);
+                    break;
+
+                case WARN:
+                    LOGGER.warn(message);
+                    break;
+
+                case OFF:
+                    LOGGER.info("Logging is OFF");
+                    break;
+
+                case TRACE:
+                    LOGGER.trace(message);
+                    break;
+
+                default:
+                case INFO:
+                    LOGGER.info(message);
+                    break;
+            }
+            //response entity in case of log is created successfully return http status OK
             return new ResponseEntity<>("logs created", HttpStatus.OK);
         }
+        //Exception response back to calling microservice
         catch(Exception e){
+            //response entity in case of exception return http status for expectation failed
             return new ResponseEntity<>("unable to creates logs because of:"+e,HttpStatus.EXPECTATION_FAILED);
         }
     }
